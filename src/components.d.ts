ff34b09c5ba22cef49750cfc60d4fbe536daf873
Johan Axelsson <2019SummerFun>
2019-06-27 13:51:51 +0300

    Ionic 4.5.0 for comparison

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,15 +5,31 @@
  */
 
 
-import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
+import '@stencil/core';
+
+import '@ionic/core';
+import 'ionicons';
 
 
 export namespace Components {
+
   interface AppHome {}
+  interface AppHomeAttributes extends StencilHTMLAttributes {}
+
   interface AppRoot {}
+  interface AppRootAttributes extends StencilHTMLAttributes {}
 }
 
 declare global {
+  interface StencilElementInterfaces {
+    'AppHome': Components.AppHome;
+    'AppRoot': Components.AppRoot;
+  }
+
+  interface StencilIntrinsicElements {
+    'app-home': Components.AppHomeAttributes;
+    'app-root': Components.AppRootAttributes;
+  }
 
 
   interface HTMLAppHomeElement extends Components.AppHome, HTMLStencilElement {}
@@ -27,29 +43,24 @@ declare global {
     prototype: HTMLAppRootElement;
     new (): HTMLAppRootElement;
   };
+
   interface HTMLElementTagNameMap {
+    'app-home': HTMLAppHomeElement
+    'app-root': HTMLAppRootElement
+  }
+
+  interface ElementTagNameMap {
     'app-home': HTMLAppHomeElement;
     'app-root': HTMLAppRootElement;
   }
-}
-
-declare namespace LocalJSX {
-  interface AppHome extends JSXBase.HTMLAttributes<HTMLAppHomeElement> {}
-  interface AppRoot extends JSXBase.HTMLAttributes<HTMLAppRootElement> {}
-
-  interface IntrinsicElements {
-    'app-home': AppHome;
-    'app-root': AppRoot;
-  }
-}
-
-export { LocalJSX as JSX };
 
 
-declare module "@stencil/core" {
   export namespace JSX {
-    interface IntrinsicElements extends LocalJSX.IntrinsicElements {}
+    export interface Element {}
+    export interface IntrinsicElements extends StencilIntrinsicElements {
+      [tagName: string]: any;
+    }
   }
+  export interface HTMLAttributes extends StencilHTMLAttributes {}
+
 }
-
-
--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State } from '@stencil/core';
+import { Component, State } from '@stencil/core';
 
 @Component({
   tag: 'app-home',
